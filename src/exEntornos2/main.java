e2be3458a39dac8828cb90c309f78246e3dadf17
franMV4 <79657510+franMV4@users.noreply.github.com>
2021-05-20 19:49:49 +0200

    todo a rama develop

---
+++ src/exEntornos2/main.java
@@ -0,0 +1,18 @@
+package exEntornos2;
+
+import com.SanValero.Menu;
+import com.SanValero.Restaurante;
+import com.SanValero.Usuario;
+
+public class main {
+
+    public static void main(String[]args){
+        Menu menu;
+        Restaurante restaurante;
+        Usuario usuario;
+
+        menu = new Menu("0", "0");
+        restaurante = new Restaurante("Jorge", "Argualas", "s", "h", "2");
+        usuario = new Usuario("Adrian", "74642478C", "g", "d");
+
+    }}
